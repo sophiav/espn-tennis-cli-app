f2d9319d8c3b7d9001bd8ecaba68e1282e41a30d
Sophia Varella <sophiavarella@gmail.com>
2017-02-18 15:29:41 -0800

    print details in the rankings and format the output

--- lib/rankings.rb
+++ lib/rankings.rb
@@ -2,7 +2,7 @@ class Rankings
   # prints all the rankings
   def self.print_rankings
     Player.all.each do |player|
-      puts "#{player.name}"
+      puts "#{player.rank.ljust(5)}#{player.name.ljust(25)}#{player.country.ljust(25)}#{player.points}"
     end
   end
 end
