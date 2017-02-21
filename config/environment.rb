36f475e58c1362a5038e39ea2818fed105ee3776
Sophia Varella <sophiavarella@gmail.com>
2017-02-20 20:01:08 -0800

    updated expand_path command to use File.dirname instead of just __FILE__ since it makes a bit more sense

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@ require 'open-uri'
 require 'require_all'
 require 'nokogiri'
 
-lib_path = File.expand_path('../../lib', __FILE__)
+lib_path = File.expand_path('../lib', File.dirname(__FILE__))
 require_all lib_path
